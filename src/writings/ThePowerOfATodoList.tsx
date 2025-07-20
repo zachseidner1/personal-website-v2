a3c1165bd419818dd1a614026d90035dd8606d3a
Zach Seidner <zachary.seidner@gmail.com>
2025-07-20 09:10:49 -0400

    progress

---
+++ src/writings/ThePowerOfATodoList.tsx
@@ -0,0 +1,31 @@
+export default function ThePowerOfATodoList() {
+  return (
+    <>
+      <p>
+        {/* Cool introduction here with good writing  */}
+        I've found a solution to this problem, that for the last couple weeks,
+        has served me wonders. A to-do list. Simply write out all that you want
+        to do, in a list. Then when you don't know what to do, consult the list.
+        Easy, right?
+      </p>
+      <p>
+        Using a to-do list, even for the "unproductive" tasks of your life,
+        helps to reframe what a "productive" day looks like. Before using a
+        to-do list, trying to have a productive day felt overwhelming. You're
+        telling me I'm supposed to work for 8 hours straight, then get home, go
+        to the gym, cook dinner, then still have energy left for writing, or
+        working on a side project?
+      </p>
+      <p>
+        Having a to-do list put things into perspective for me. If I didn't feel
+        like doing a certain task, I could just do a different one. The other
+        task is still on the list. I'll get to it eventually.
+      </p>
+      <p>
+        There's also the satisfaction of being able to cross items off the list.
+        Then at the end of the day you can look back at what you accomplished,
+        satisfied.
+      </p>
+    </>
+  );
+}
