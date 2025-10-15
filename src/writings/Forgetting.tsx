37a7defed11c5ebd36ce3fda4df45bcc1f0ec388
zachseidner1 <zachary.seidner@gmail.com>
2025-10-15 14:54:34 -0400

    fix grammatical error that annoyed me

--- src/writings/Forgetting.tsx
+++ src/writings/Forgetting.tsx
@@ -3,7 +3,7 @@ export default function Forgetting() {
     <div>
       <p>Today I realized I forgot how to do u-substitution</p>
       <p>
-        It made me realize that eventually there will be a day where I forget
+        It made me realize that eventually there will be a day when I forget
         whether the derivative of sine is negative or positive cosine
       </p>
       <p>This makes me a little sad</p>
